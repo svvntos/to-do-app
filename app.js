6cb7dcdfd47aa5d7f2d77f6880c5f44910e15eae
Joe Santos <jaysantos012@gmail.com>
2021-06-03 00:00:43 -0400

    adding the localstorage to the app. was able to set item now working on the function to get item

--- app.js
+++ app.js
@@ -6,28 +6,54 @@ formEL.addEventListener('submit', (e) => {
 
     e.preventDefault();
 
+    addTodo();
+
+
+})
+
+function addTodo() {
     const todoText = inputEL.value;
 
     if (todoText) {
         const newToDo = document.createElement('li');
         newToDo.innerText = todoText;
 
-    
-    
+
+
         newToDo.addEventListener('click', () => {
             newToDo.classList.toggle('completed');
+
+            updateLS();
         })
 
-        newToDo.addEventListener("contextmenu" || "touchend", (e) =>{
+        newToDo.addEventListener("contextmenu" || "touchend", (e) => {
             e.preventDefault();
             newToDo.remove();
+            updateLS();
         })
- 
 
-todoEL.appendChild(newToDo);
 
-inputEL.value = '';
+        todoEL.appendChild(newToDo);
+
+        inputEL.value = '';
+
+        updateLS();
     }
+}
 
 
-})
+function updateLS() {
+
+        const newTodoEL = document.querySelectorAll('li');
+
+        const todos = [];
+
+        newTodoEL.forEach((newTodoEL) => {
+            todos.push({
+                text: newTodoEL.innerHTML,
+                completed: newTodoEL.classList.contains("completed"),
+            });
+        });
+
+        localStorage.setItem('todos', JSON.stringify(todos));
+}
